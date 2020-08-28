6daef52c72656c2c35f8143672e3efaf446db17b
Cesar Morales <ionlinesolutions.com@gmail.com>
2020-08-28 10:16:25 -0500

    Change profile view buttons

--- lib/platzi_trips_cupertino.dart
+++ lib/platzi_trips_cupertino.dart
@@ -5,14 +5,15 @@ import 'package:platzi_viajes/Place/ui/screens/search_trips.dart';
 import 'package:platzi_viajes/User/ui/screens/profile_trips.dart';
 
 class PlatziTripsCupertino extends StatelessWidget {
+  final String name;
+
+  PlatziTripsCupertino({Key key, @required this.name}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: CupertinoTabScaffold(
-        
-        tabBar: CupertinoTabBar(
-          backgroundColor: Colors.transparent,
-          items: [
+        tabBar: CupertinoTabBar(backgroundColor: Colors.transparent, items: [
           BottomNavigationBarItem(
               icon: Icon(Icons.home, color: Colors.indigo), title: Text("")),
           BottomNavigationBarItem(
--- lib/user/ui/widgets/button_bar.dart
+++ lib/user/ui/widgets/button_bar.dart
@@ -12,11 +12,21 @@ class ButtonsBar extends StatelessWidget {
         ),
         child: Row(
           children: <Widget>[
-            CircleButton(true, Icons.turned_in_not, 20.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(true, Icons.card_travel, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
-            CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(true, Icons.mail_outline, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
-            CircleButton(true, Icons.person, 20.0, Color.fromRGBO(255, 255, 255, 0.6))
+            // Write a post
+            CircleButton(true, Icons.turned_in_not, 20.0, Color.fromRGBO(255, 255, 255, 1),
+              () => {}),
+            // Change password
+            CircleButton(true, Icons.vpn_key, 20.0, Color.fromRGBO(255, 255, 255, 0.6),
+              () => {}),
+            // Add new Place
+            CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1),
+              () => {}),
+            // Send a new email
+            CircleButton(true, Icons.mail_outline, 20.0, Color.fromRGBO(255, 255, 255, 0.6),
+              () => {}),
+            // Loggout app
+            CircleButton(true, Icons.exit_to_app, 20.0, Color.fromRGBO(255, 255, 255, 0.6),
+              () => {})
           ],
         )
     );
